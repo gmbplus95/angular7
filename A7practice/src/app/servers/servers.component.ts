3ea71bab58d5208b225ff111c3f8edaf95304596
BacNV <bac.nguyenvan@ifisolution.com>
2019-01-04 08:58:49 +0700

    04-09

--- A7practice/src/app/servers/servers.component.ts
+++ A7practice/src/app/servers/servers.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '[app-servers]', //directive
+  // selector: '.app-servers', //class
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
